9e16294ab42e58672c6c64f119a755f5e1228551
Abinash <45118878+abinashunix@users.noreply.github.com>
2023-04-20 03:05:24 +0530

    Update ecs.tf

--- ecs.tf
+++ ecs.tf
@@ -56,7 +56,7 @@ resource "aws_ecs_task_definition" "ecs_task_definition" {
 [
   {
     "name": "my-container",
-    "image": "${data.aws_ecr_repository.my_repository.repository_url}:latest",
+    "image": "${data.aws_ecr_repository.my_repository.repository_url}",
     
     "portMappings": [
       {
