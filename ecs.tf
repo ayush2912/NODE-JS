37d2029d1896a08c31fef1e0108ab535b3f4fa9f
ayush2912 <76970440+ayush2912@users.noreply.github.com>
2023-04-19 16:50:14 +0530

    Update ecs.tf

--- ecs.tf
+++ ecs.tf
@@ -86,7 +86,7 @@ resource "aws_ecs_service" "my_service" {
 
   network_configuration {
     security_groups = [aws_security_group.ecs_security_group.id]
-    subnets         = data.aws_subnet.my_subnet_ids.id
+    subnets         = [data.aws_subnet.my_subnet_ids.id]
 
    
   }
