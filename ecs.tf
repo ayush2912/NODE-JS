a939172cf5e2389f55399b61f5cfb6a1ae162f4e
Abinash <45118878+abinashunix@users.noreply.github.com>
2023-04-20 00:56:22 +0530

    Update ecs.tf

--- ecs.tf
+++ ecs.tf
@@ -73,6 +73,22 @@ resource "aws_ecs_task_definition" "ecs_task_definition" {
 ]
 DEFINITION
 }
+resource "aws_iam_role" "ecsTaskExecutionRole" {
+  name               = "ecsTaskExecutionRole"
+  assume_role_policy = "${data.aws_iam_policy_document.assume_role_policy.json}"
+}
+
+data "aws_iam_policy_document" "assume_role_policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["ecs-tasks.amazonaws.com"]
+    }
+  }
+}
+
 resource "aws_ecs_service" "my_service" {
   name            = "my-service"
   cluster         = aws_ecs_cluster.ecs_cluster.id
