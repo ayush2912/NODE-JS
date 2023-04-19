1f833a98c5e2bb32b1fc4e36247ce66ec5beee7b
Abinash <45118878+abinashunix@users.noreply.github.com>
2023-04-20 00:28:54 +0530

    Update ecs.tf

--- ecs.tf
+++ ecs.tf
@@ -40,17 +40,10 @@ data "aws_ecr_repository" "my_repository" {
   name = "my-repository"
 }
 # Define the ECS task definition
-resource "aws_ecs_task_definition" "ecs_task_definition" {
+resource "aws_ecs_task_definition" "my_first_task" {
   family                   = "my-ecs-task"
-  requires_compatibilities = ["FARGATE"]
-  network_mode             = "awsvpc"
-  cpu                      = "1 vCPU"
-  memory                   = "2048"
-  execution_role_arn       = "arn:aws:iam::168933414344:role/ecsTaskExecutionRole"
-  
-
   container_definitions = <<DEFINITION
-[
+ [
   {
     "name": "my-container",
     "image": "${data.aws_ecr_repository.my_repository.repository_url}:latest",
@@ -70,9 +63,36 @@ resource "aws_ecs_task_definition" "ecs_task_definition" {
       }
     }
   }
+  ]
+  DEFINITION
+  requires_compatibilities = ["FARGATE"]
+  network_mode             = "awsvpc"
+  cpu                      = "1 vCPU"
+  memory                   = "2048"
+  execution_role_arn       = "arn:aws:iam::168933414344:role/ecsTaskExecutionRole"
+  
+[
+  {
+    "name": "my-ecs-task",
+    "image": "${data.aws_ecr_repository.my_repository.repository_url}:latest",
+    
+    "portMappings": [
+      {
+        "containerPort": 3000,
+        "protocol": "tcp"
+      }
+    ],
+    "logConfiguration": {
+      "logDriver": "awslogs",
+      "options": {
+        "awslogs-group": "/ecs/my-ecs-task",
+        "awslogs-region": "ap-south-1",
+        "awslogs-stream-prefix": "my-container"
+      }
+    }
+  }
 ]
 DEFINITION
-}
 resource "aws_ecs_service" "my_service" {
   name            = "my-service"
   cluster         = aws_ecs_cluster.ecs_cluster.id
@@ -83,6 +103,25 @@ resource "aws_ecs_service" "my_service" {
   deployment_controller {
     type = "ECS"
   }
+  
+  resource "aws_iam_role" "ecsTaskExecutionRole" {
+  name               = "ecsTaskExecutionRole"
+  assume_role_policy = "${data.aws_iam_policy_document.assume_role_policy.json}"
+}
+  data "aws_iam_policy_document" "assume_role_policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["ecs-tasks.amazonaws.com"]
+    }
+  }
+}
+  resource "aws_iam_role_policy_attachment" "ecsTaskExecutionRole_policy" {
+  role       = "${aws_iam_role.ecsTaskExecutionRole.name}"
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
 
   network_configuration {
     security_groups = [aws_security_group.ecs_security_group.id]
