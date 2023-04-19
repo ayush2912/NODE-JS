b53a364e2c7a717e19d5968f3bc4b4e80ff5a3a4
ayush2912 <76970440+ayush2912@users.noreply.github.com>
2023-04-19 12:24:30 +0530

    Update ecs.tf

--- ecs.tf
+++ ecs.tf
@@ -36,7 +36,7 @@ resource "aws_ecs_task_definition" "ecs_task_definition" {
     "portMappings": [
       {
         "containerPort": 3000,
-        "hostPort": 80
+        "hostPort": 0
       }
     ],
     "logConfiguration": {
