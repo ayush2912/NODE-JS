c852ea6aad8fe1e71c6b5b9ca4e8702e636b7367
ayush2912 <76970440+ayush2912@users.noreply.github.com>
2023-04-21 15:59:16 +0530

    Update ecf.tf

--- ecf.tf
+++ ecf.tf
@@ -20,7 +20,7 @@ name_prefix = "ayush-new"
  vpc_id      = data.aws_vpc.existing_vpc.id
   ingress {
     from_port   = 0
-    to_port     = 0
+    to_port     = 65535
     protocol    = "tcp"
     security_groups = ["sg-04c53a4831e9f6ff8"]
   }
